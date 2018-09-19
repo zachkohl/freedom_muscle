3f01aca6e37011b4982bde379af17ff8d0354b26
zachkohl <zach.kohl@gmail.com>
2018-09-19 12:15:07 -0700

    manual convert to pacific time

--- app.js
+++ app.js
@@ -586,7 +586,7 @@ app.post('/deletegoal',checkSession,function(req, res){
 
 app.post('/ajaxreviewtime',checkSession,function(req, res){
   let text = 'UPDATE users SET reviewtime = $1 WHERE (username = $2)'
-  let reviewTime = new Date(req.body.reviewTime).toDateString()
+  let reviewTime = new Date(req.body.reviewTime - 7*60*1000).toDateString()
   console.log(reviewTime)
   let values = [reviewTime,req.session.user]
   console.log(req.body.reviewTime)
