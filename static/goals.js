5696726b844ea1d8258dae7938796a655ca0fadb
zachkohl <zach.kohl@gmail.com>
2018-09-19 12:06:33 -0700

    trying this again

--- static/goals.js
+++ static/goals.js
@@ -155,7 +155,6 @@ else{
 $( '#master' ).on("click",".submission",function(){  //must use .on() if you want to update updates. 
     let package = $(this).parents('form').serializeArray();
     let timestamp = new Date();
-    timestamp = new Date(timestamp.getTime() + timestamp.getTimezoneOffset()*60*1000)
     console.log(timestamp.toJSON());
     console.log(timestamp)
     package.push({name:'timestamp', value:timestamp}); 
