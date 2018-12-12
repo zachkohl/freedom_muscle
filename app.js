b803e404674ce1128f76c0cbfdca92fc2f97843d
zachkohl <zach.kohl@gmail.com>
2018-12-11 19:30:20 -0800

    trying this again

--- app.js
+++ app.js
@@ -162,7 +162,7 @@ var sess = {
      // Use another table-name than the default "session" one
   }),
   secret: 'keyboard cat',
-  cookie: {},
+  cookie: {maxAge:60000*60*24*7},
   resave: false,
   saveUninitialized: true
 }
